b1732de95f361085af14eceeb3be77827afedf70
Aleksandr Birukov <liznitopor48@gmail.com>
2016-04-06 23:12:53 +0300

    liznitopor48

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "myclass.h"
+using namespace std;
+
+int main()
+{
+    myclass a(10, 10, 10), b, c, d;
+    double m;
+    b.input();
+    c=b.plus(a);
+    d=b.minus(a);
+    b.mod();
+    c.show();
+    d.show();
+    return 0;
+}
+
+
